b5f4c705163f5c42f4e94cc548ef986ce9a4ab94
Rodrigo Vargas <42876568+ravargas111@users.noreply.github.com>
2018-08-31 17:48:04 -0600

    dsfsdf

--- PruebaGitHub/src/pruebagithub/PruebaGitHub.java
+++ PruebaGitHub/src/pruebagithub/PruebaGitHub.java
@@ -22,7 +22,7 @@ public class PruebaGitHub extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Rama 1 clon");
+        btn.setText("asdasdsa");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
