41a231a1a3158bcdbb76764479ffae13f26d8fe8
Rodrigo Vargas <42876568+ravargas111@users.noreply.github.com>
2018-08-31 15:06:26 -0600

    asd

--- PruebaGitHub/src/pruebagithub/PruebaGitHub.java
+++ PruebaGitHub/src/pruebagithub/PruebaGitHub.java
@@ -22,7 +22,7 @@ public class PruebaGitHub extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Say 'branch 2'");
+        btn.setText("Rama 2");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
