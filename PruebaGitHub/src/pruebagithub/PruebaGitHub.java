974f10320d697654982c94261fd06ab0fd3f9d8a
Rodrigo Vargas <42876568+ravargas111@users.noreply.github.com>
2018-08-31 18:01:56 -0600

    rytu

--- PruebaGitHub/src/pruebagithub/PruebaGitHub.java
+++ PruebaGitHub/src/pruebagithub/PruebaGitHub.java
@@ -24,6 +24,7 @@ public class PruebaGitHub extends Application {
         Button btn = new Button();
         btn.setText("cambio");
         System.out.println("nueva 1");
+>>>>>>> 999d265bbe1c36344e300ed05061cf1e9e3a9495
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
